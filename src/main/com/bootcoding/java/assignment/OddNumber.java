40fc727217674b054f219fdc9c67f679b80d453f
DhirajMandal16 <dhiraj.mandal.bca@ghrietn.raisoni.net>
2023-01-03 21:24:40 +0530

    Program to find odd number

--- src/main/com/bootcoding/java/assignment/OddNumber.java
+++ src/main/com/bootcoding/java/assignment/OddNumber.java
@@ -2,7 +2,7 @@ package com.bootcoding.java.assignment;
 
 public class OddNumber {
     public static void main(String[] args) {
-        int n=10;
+        int n=15;
         for(int i=2;i<=n;i++){
             if(i%2!=0){
                 System.out.println(i);
