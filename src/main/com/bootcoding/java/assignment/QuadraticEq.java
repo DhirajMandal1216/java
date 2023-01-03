e5a1913fb06bff548b58a0f17b7081ebf3f7dec9
DhirajMandal16 <dhiraj.mandal.bca@ghrietn.raisoni.net>
2023-01-03 22:42:09 +0530

    Program to find result of Quadratic Equation.

---
+++ src/main/com/bootcoding/java/assignment/QuadraticEq.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.assignment;
+
+public class QuadraticEq {
+    public static void main(String[] args) {
+        int a=2,b=5;
+        int c=(a*a)+(b*b)+(2*a*b)+((a*b)*(a*b));
+        System.out.println(c);
+    }
+}
